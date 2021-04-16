b17245342d46ea7dc54efa9678d2be9590d0a2d6
miyabihito <miyabihito.dev@gmail.com>
2021-04-16 17:59:53 +0900

    add sort test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
 pub fn sort(input: &mut Vec<i32>) {
     input.sort();
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_sort_asc() {
+        let mut input = vec![3, 2, 1];
+        sort(&mut input);
+
+        assert_eq!(vec![1, 2, 3], input);
+    }
+}
